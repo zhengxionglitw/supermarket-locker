f4e27537e03100207b183be18eb3a9ae6ec59b67
zhengxiongli <xlzheng@thoughtworks.com>
2020-07-03 10:48:18 +0800

    zhengxiongli 当储物柜已满时存包失败

--- src/main/java/com/thoughtworks/locker/Locker.java
+++ src/main/java/com/thoughtworks/locker/Locker.java
@@ -1,19 +1,26 @@
 package com.thoughtworks.locker;
 
 
+import com.thoughtworks.locker.exception.FullException;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Locker {
-    private final int capacity;
+    private final AtomicInteger availableCapacity;
     private final Map<Ticket, Bag> storeBags = new HashMap<>();
 
     public Locker(int capacity) {
         assert (capacity > 0);
-        this.capacity = capacity;
+        this.availableCapacity = new AtomicInteger(capacity);
     }
 
     public Ticket store(Bag bag) {
+        if (availableCapacity.get() == 0) {
+            throw new FullException();
+        }
+        availableCapacity.decrementAndGet();
         Ticket ticket = new Ticket();
         storeBags.put(ticket, bag);
         return ticket;
