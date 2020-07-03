4b42cf2dd69c827eb0fee89dccee85d2ea39002e
zhengxiongli <xlzheng@thoughtworks.com>
2020-07-03 10:38:44 +0800

    zhengxiongli locker有容量时，成功存包，返回ticket

--- src/main/java/com/thoughtworks/locker/Locker.java
+++ src/main/java/com/thoughtworks/locker/Locker.java
@@ -1,10 +1,20 @@
 package com.thoughtworks.locker;
 
+
+import java.util.HashMap;
+
 public class Locker {
     private final int capacity;
+    private final HashMap<Ticket, Bag> storeBags = new HashMap<>();
 
     public Locker(int capacity) {
         assert (capacity > 0);
         this.capacity = capacity;
+    }
+
+    public Ticket store(Bag bag) {
+        Ticket ticket = new Ticket();
+        storeBags.put(ticket, bag);
+        return ticket;
     }
 }
